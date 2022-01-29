77f97062ad9d0b521e25e80c455e5727d30bf232
Denis Woolley (CreepyD) <denisjnr88@gmail.com>
2022-01-29 09:46:30 +0200

    Update ShapeDetection.py

--- ShapeDetection.py
+++ ShapeDetection.py
@@ -1,7 +1,7 @@
 import cv2 # OpenCV Library
 
 # Image to detect shapes on below
-image = cv2.imread("C:\\Users\\denis\\Desktop\\img\\shape.jpg")
+image = cv2.imread("INSERT PATH TO IMAGE FILE HERE")
 
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Converting to gray image
 
